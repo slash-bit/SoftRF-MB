d13a990e7ffc6ce3a041501892a1a3122c7aefd1
slash-bit <v-b2@live.com>
2026-06-17 21:31:34 +0100

    Added PG-hill-filter setting to Alarm options

--- software/firmware/source/SoftRF/src/protocol/data/JSON.cpp
+++ software/firmware/source/SoftRF/src/protocol/data/JSON.cpp
@@ -1055,6 +1055,7 @@ void parseJSettings(JsonObject root)
     else if (!strcmp(s,"DISTANCE")) settings->alarm = TRAFFIC_ALARM_DISTANCE;
     else if (!strcmp(s,"VECTOR"))   settings->alarm = TRAFFIC_ALARM_VECTOR;
     else if (!strcmp(s,"LATEST"))   settings->alarm = TRAFFIC_ALARM_LATEST;
+    else if (!strcmp(s,"PG_HILL"))  settings->alarm = TRAFFIC_ALARM_PG_HILL;
   }
 
   key = "alarm_filter";
@@ -1260,7 +1261,8 @@ bool writeJSettings(JsonObject obj)
     (settings->alarm == TRAFFIC_ALARM_NONE)     ? "NONE"     :
     (settings->alarm == TRAFFIC_ALARM_DISTANCE) ? "DISTANCE" :
     (settings->alarm == TRAFFIC_ALARM_VECTOR)   ? "VECTOR"   :
-    (settings->alarm == TRAFFIC_ALARM_LATEST)   ? "LATEST"   : "VECTOR";
+    (settings->alarm == TRAFFIC_ALARM_LATEST)   ? "LATEST"   :
+    (settings->alarm == TRAFFIC_ALARM_PG_HILL)  ? "PG_HILL"  : "LATEST";
 
   obj["alarm_filter"] = "NONE";   /* not yet implemented in Moshe settings_t */
 
